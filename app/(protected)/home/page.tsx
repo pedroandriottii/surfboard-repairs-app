cfa13bf72971a3e1b9e6112b30e839719e8a3533
Pedro Andriotti <phab@cesar.school>
2024-03-28 19:31:28 -0300

    Visualize my services feature completed

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -4,16 +4,51 @@ import { useCurrentRole } from '@/hooks/use-current-role';
 import { useCurrentUser } from '@/hooks/use-current-user';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { getAllServices, getServicesByEmail } from '@/data/services';
+import { Service } from '@prisma/client';
 
 const HomePage = () => {
     const role = useCurrentRole();
     const user = useCurrentUser();
-    const [services, setServices] = useState();
+    const [services, setServices] = useState <Service[] | null>();
+
+    if(role === "USER") {
+        useEffect(() => {
+            const getServices = async () => {
+              const services = await getServicesByEmail(user?.email!!)
+              setServices(services);
+            }
+        
+            getServices();
+        }, [user?.email])
+    }
+    if(role === "ADMIN"){
+        useEffect(() => {
+            const getServices = async () => {
+              const services = await getAllServices()
+              setServices(services);
+            }
+        
+            getServices();
+        }, [user?.email])
+    }
+    
+
+
 
     if(role === "ADMIN"){
         return (
             <div>
                 <h1>Olá {user?.name}</h1>
+                {services?.map((service) => {
+                    return (
+                        <div key={service.id}>
+                            <h2>{service.client_name}</h2>
+                            <p>{service.description}</p>
+                        </div>
+                    )
+                })
+                }
                 <Button variant="default">
                     <Link href="/create-service">Registar Conserto</Link>
                 </Button>
@@ -22,8 +57,17 @@ const HomePage = () => {
     }
     if(role === "USER"){
         return (
-            <div>Olá {user?.name}
-
+            <div>
+                <h1>Olá {user?.name}</h1>
+                {services?.map((service) => {
+                    return (
+                        <div key={service.id}>
+                            <h2>{service.client_name}</h2>
+                            <p>{service.description}</p>
+                        </div>
+                    )
+                })
+                }
             </div>
         )
     }
--- data/services.ts
+++ data/services.ts
@@ -1,21 +1,28 @@
+"use server";
 import { db } from "@/lib/db";
 
-// export const getServicesByEmail = async (email: string) => {
-//     try{
-//         const Service = await db.service.findUnique({ where: { email } });
-        
-//         return service;
-//     }catch{
-//         return null;
-//     }
-// }
-
-export const getUserById = async (id: string) => {
+export const getServicesByEmail = async (email: string) => {
     try{
-        const user = await db.user.findUnique({ where: { id } });
-        
-        return user;
-    }catch{
+        const services = await db.service.findMany({
+            where: {
+                user_mail: email,
+            },
+        });
+        return services;
+    } catch (error) {
+        console.error("Erro ao buscar serviços pelo email do usuário", error)
+        return null;
+    }
+
+}
+
+export const getAllServices = async () => {
+    try{
+        const services = await db.service.findMany();
+        return services;
+    }
+    catch(error){
+        console.error("Erro ao buscar serviços", error);
         return null;
     }
 }
