67212b016d089e445623ccdb9da879e38d7baf36
Pedro Henrique Andriotti Bastos <phab@cesar.school>
2024-08-14 15:50:54 -0300

    adding backgroundImage to create service component

--- components/services/create-service-form.tsx
+++ components/services/create-service-form.tsx
@@ -30,7 +30,7 @@ import { createService } from "@/actions/create-service";
 import { RoleGate } from "../auth/role-gate";
 import Navbar from "../base/navbar";
 import { Textarea } from "../ui/textarea";
-import { Label } from "../ui/label";
+import BackgroundImage from "../base/backgroundImage";
 
 export const CreateServiceForm = () => {
     const [error, setError] = useState<string | undefined>("");
@@ -105,97 +105,101 @@ export const CreateServiceForm = () => {
     };
 
     return (
-        <div className="flex flex-col w-full h-full bg-black">
-            <div className='flex justify-between w-full'>
-                <Navbar role={role} />
-            </div>
-            <h1 className="text-realce text-xl font-bold py-2 text-center">Cadastrar Serviço</h1>
-            <ToastContainer position="top-center" autoClose={9000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
-            <RoleGate allowedRoles={['ADMIN', 'MASTER']}>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 text-black p-4">
-                        <div className="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-4">
-                            <FormField control={form.control} name="photo_url" render={({ field }) => {
-                                const { value, ...inputProps } = field;
-                                return (
+        <div className="relative w-full flex flex-col min-h-screen overflow-x-hidden">
+            <BackgroundImage src="/splash.png" alt="Background" />
+            <BackgroundImage src="/splash_desk.png" alt="Background" isDesktop />
+            <div className="relative z-20 flex flex-col items-center w-full">
+                <div className='flex justify-between w-full'>
+                    <Navbar role={role} />
+                </div>
+                <ToastContainer position="top-center" autoClose={9000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+                <RoleGate allowedRoles={['ADMIN', 'MASTER']}>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 text-black p-4">
+                            <h1 className="text-realce w-full py-2 rounded-xl text-xl font-bold text-center bg-transparent border border-realce">Cadastrar Serviço</h1>
+                            <div className="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-4">
+                                <FormField control={form.control} name="photo_url" render={({ field }) => {
+                                    const { value, ...inputProps } = field;
+                                    return (
+                                        <FormItem>
+                                            <FormLabel className="text-realce">Foto da Prancha</FormLabel>
+                                            <FormControl>
+                                                <Input {...inputProps} type="file" disabled={isPending} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    );
+                                }} />
+                                <FormField control={form.control} name="client_name" render={({ field }) => (
                                     <FormItem>
-                                        <FormLabel className="text-realce">Foto da Prancha</FormLabel>
+                                        <FormLabel className="text-realce">Nome do Cliente</FormLabel>
                                         <FormControl>
-                                            <Input {...inputProps} type="file" disabled={isPending} />
+                                            <Input {...field} placeholder="João Silva" type="name" disabled={isPending} />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
-                                );
-                            }} />
-                            <FormField control={form.control} name="client_name" render={({ field }) => (
+                                )} />
+                                <FormField control={form.control} name="user_mail" render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-realce">Email do Cliente</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} placeholder="exemplo@email.com" type="email" disabled={isPending} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )} />
+                                <FormField control={form.control} name="phone" render={({ field }) => (
+                                    <FormItem className="flex flex-col gap-2">
+                                        <FormLabel className="text-realce">Telefone do Cliente</FormLabel>
+                                        <FormControl>
+                                            <InputMask mask="+5\5 (99) 99999-9999" {...field} className="border rounded-md p-2 w-full" placeholder='+55 (99) 99999-9999' required disabled={isPending} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )} />
+                                <FormField control={form.control} name="value" render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-realce">Valor do serviço</FormLabel>
+                                        <FormControl>
+                                            <div className="flex items-center border w-full rounded-md h-10 border-input pl-3">
+                                                <span className="mr-2 text-white">R$</span>
+                                                <Input {...field} placeholder="Valor" type="number" className="flex-1" disabled={isPending} value={field.value || ''} onChange={e => form.setValue('value', e.target.valueAsNumber)} />
+                                            </div>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )} />
+                                <FormField control={form.control} name="max_time" render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-realce">Prazo</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} type="date" disabled={isPending} onChange={(e) => { const selectedDate = new Date(e.target.value); form.setValue("max_time", selectedDate); }} value={field.value ? field.value.toISOString().substring(0, 10) : ''} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
+                            </div>
+                            <FormField control={form.control} name="description" render={({ field }) => (
                                 <FormItem>
-                                    <FormLabel className="text-realce">Nome do Cliente</FormLabel>
+                                    <FormLabel className="text-realce">Descrição do serviço</FormLabel>
                                     <FormControl>
-                                        <Input {...field} placeholder="João Silva" type="name" disabled={isPending} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )} />
-                            <FormField control={form.control} name="user_mail" render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-realce">Email do Cliente</FormLabel>
-                                    <FormControl>
-                                        <Input {...field} placeholder="exemplo@email.com" type="email" disabled={isPending} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )} />
-                            <FormField control={form.control} name="phone" render={({ field }) => (
-                                <FormItem className="flex flex-col gap-2">
-                                    <FormLabel className="text-realce">Telefone do Cliente</FormLabel>
-                                    <FormControl>
-                                        <InputMask mask="+5\5 (99) 99999-9999" {...field} className="border rounded-md p-2 w-full" placeholder='+55 (99) 99999-9999' required disabled={isPending} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )} />
-                            <FormField control={form.control} name="value" render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-realce">Valor do serviço</FormLabel>
-                                    <FormControl>
-                                        <div className="flex items-center border w-full rounded-md h-10 border-input pl-3">
-                                            <span className="mr-2 text-white">R$</span>
-                                            <Input {...field} placeholder="Valor" type="number" className="flex-1" disabled={isPending} value={field.value || ''} onChange={e => form.setValue('value', e.target.valueAsNumber)} />
+                                        <div className="grid w-full gap-1.5">
+                                            <Textarea className="bg-background" {...field} placeholder="Descrição" id="message" disabled={isPending} />
                                         </div>
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
                             )} />
-                            <FormField control={form.control} name="max_time" render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-realce">Prazo</FormLabel>
-                                    <FormControl>
-                                        <Input {...field} type="date" disabled={isPending} onChange={(e) => { const selectedDate = new Date(e.target.value); form.setValue("max_time", selectedDate); }} value={field.value ? field.value.toISOString().substring(0, 10) : ''} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                            />
-                        </div>
-                        <FormField control={form.control} name="description" render={({ field }) => (
-                            <FormItem>
-                                <FormLabel className="text-realce">Descrição do serviço</FormLabel>
-                                <FormControl>
-                                    <div className="grid w-full gap-1.5">
-                                        <Textarea className="bg-background" {...field} placeholder="Descrição" id="message" disabled={isPending} />
-                                    </div>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )} />
-                        <FormError message={error} />
-                        <FormSuccess message={success} />
-                        <Button type="submit" className="w-full bg-realce text-black hover:bg-white" disabled={isPending}>
-                            Confirmar
-                        </Button>
-                    </form>
-                </Form>
-            </RoleGate>
+                            <FormError message={error} />
+                            <FormSuccess message={success} />
+                            <Button type="submit" className="w-full bg-realce text-black hover:bg-white" disabled={isPending}>
+                                Cadastrar
+                            </Button>
+                        </form>
+                    </Form>
+                </RoleGate>
+            </div>
         </div>
     );
 };
