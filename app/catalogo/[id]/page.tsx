d782ee9d3c59bd04da74d61b01599c493562b6b3
Pedro Andriotti <phab@cesar.school>
2024-08-20 17:37:46 -0300

    details surfboard page mobile completed

--- app/catalogo/[id]/page.tsx
+++ app/catalogo/[id]/page.tsx
@@ -2,6 +2,13 @@
 import { usePathname } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import { Badge } from '@/components/ui/badge';
+import Footer from '@/components/base/footer';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import Link from 'next/link';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 interface Surfboard {
   id: string;
@@ -22,17 +29,20 @@ const Page: React.FC = () => {
   const id = pathName.replace('/catalogo/', '');
   const [surfboard, setSurfboard] = useState<Surfboard | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+  const [images, setImages] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchSurfboard = async () => {
       try {
-        console.log(id);
         const response = await fetch(`/api/marketplace/surfboards/${id}`);
         if (!response.ok) {
           throw new Error('Erro ao buscar a prancha de surf');
         }
         const data = await response.json();
         setSurfboard(data);
+        setCurrentImage(data.coverImage);
+        setImages([data.coverImage, ...data.image]);
       } catch (error) {
         if (error instanceof Error) {
           setError(error.message);
@@ -47,6 +57,26 @@ const Page: React.FC = () => {
     }
   }, [id]);
 
+  const handleImageClick = (image: string) => {
+    setCurrentImage(image);
+  };
+
+  const handleNextImage = () => {
+    if (currentImage) {
+      const currentIndex = images.indexOf(currentImage);
+      const nextIndex = (currentIndex + 1) % images.length;
+      setCurrentImage(images[nextIndex]);
+    }
+  };
+
+  const handlePreviousImage = () => {
+    if (currentImage) {
+      const currentIndex = images.indexOf(currentImage);
+      const previousIndex = (currentIndex - 1 + images.length) % images.length;
+      setCurrentImage(images[previousIndex]);
+    }
+  };
+
   if (error) {
     return <p className="text-red-500">{error}</p>;
   }
@@ -56,45 +86,85 @@ const Page: React.FC = () => {
   }
 
   return (
-    <div className='bg-black w-full h-full p-4'>
-      <Image
-        src={'/realce_logo.png'}
-        alt='Realce Surfboards'
-        width={50}
-        height={50}
-      />
-      <div >
-        <div className='flex flex-col items-center gap-4'>
-          <Image
-            src={surfboard.coverImage}
-            alt={surfboard.title}
-            width={300}
-            height={300}
-            className='rounded-xl object-cover w-72 h-72'
-          />
-          <div className='flex items-center gap-2'>
-            {surfboard.image?.map((image, index) => (
-              <Image
-                key={index}
-                src={image}
-                alt={`Imagem ${index + 1} de ${surfboard.title}`}
-                width={50}
-                height={50}
-                className='rounded-xl object-cover w-16 h-16 bg-black/80'
-              />
-            ))}
+    <div className="flex flex-col min-h-screen bg-black">
+      <div className="p-4">
+        <Image
+          src={'/realce_logo.png'}
+          alt="Realce Surfboards"
+          width={50}
+          height={50}
+        />
+      </div>
+      <div className="flex-grow">
+        <div className="py-2">
+          <Link href="/" className="bg-realce text-black py-1 px-6 rounded-r-2xl">
+            <ArrowBackIcon />
+            Voltar
+          </Link>
+        </div>
+        <div className="p-4">
+          <div className="flex flex-col items-center gap-4 relative">
+            <div className="flex items-center gap-4">
+              <button
+                onClick={handlePreviousImage}
+                className="bg-white/80 rounded-full text-black text-center items-center justify-center align-center"
+              >
+                <ArrowLeftIcon fontSize="large" />
+              </button>
+              <div className="relative">
+                <Image
+                  src={currentImage || surfboard.coverImage}
+                  alt={surfboard.title}
+                  width={300}
+                  height={300}
+                  className="rounded-xl object-cover w-72 h-72"
+                />
+              </div>
+              <button
+                onClick={handleNextImage}
+                className="bg-white/80 rounded-full text-black text-center items-center justify-center align-center"
+              >
+                <ArrowRightIcon fontSize="large" />
+              </button>
+            </div>
+            <div className="flex items-center gap-2 mt-4">
+              {images.map((image, index) => (
+                <Image
+                  key={index}
+                  src={image}
+                  alt={`Imagem ${index + 1} de ${surfboard.title}`}
+                  width={50}
+                  height={50}
+                  className={`rounded-xl object-cover w-16 h-16 bg-black/80 cursor-pointer ${currentImage === image ? 'shadow-lg shadow-white/60' : 'opacity-60'}`}
+                  onClick={() => handleImageClick(image)}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-2">
+            <h1 className="text-white text-2xl font-bold mt-4 text-center">
+              {surfboard.title}
+            </h1>
+            <Badge variant="secondary">Usada</Badge>
+            <p className="text-realce text-2xl font-bold">
+              R$ {surfboard.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
+            </p>
+            <button className="border-[1px] text-white border-white px-6 py-1 rounded-xl flex items-center gap-2">
+              <WhatsAppIcon />
+              <p className="">Entre em contato conosco!</p>
+            </button>
+          </div>
+          <div className="flex flex-col p-4 gap-3">
+            <p className="text-white font-bold">Marca: {surfboard.branding.name}</p>
+            <p className="text-white font-bold">Tamanho: {surfboard.size}</p>
+            <p className="text-white font-bold">Volume: {surfboard.volume}L</p>
+            <p className="text-white text-justify">
+              <span className="font-bold">Descrição:</span> {surfboard.description}
+            </p>
           </div>
         </div>
-
-        <h1 className='text-white text-2xl font-bold mt-4'>{surfboard.title}</h1>
-        <p className='text-white'>{surfboard.description}</p>
-        <p className='text-white'>Marca: {surfboard.branding.name}</p>
-        <p className='text-white'>Tamanho: {surfboard.size}L</p>
-        <p className='text-white'>Volume: {surfboard.volume}L</p>
-        <p className='text-white text-xl'>
-          Preço: R$ {surfboard.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
-        </p>
       </div>
+      <Footer />
     </div>
   );
 };
